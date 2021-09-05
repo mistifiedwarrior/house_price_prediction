330ff14cad4dc8ceb876f3198236d7354bbf8763
Shivam Rajput <shivam.rajput@thoughtworks.com>
2021-09-05 15:20:33 +0530

    [#2] | [Shivi/Aadii] | Added flask_cors for cors handling

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 
 from core import get_training_data, predict_price, train_model
 
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
 print("Training Model ...")
 X, Y = get_training_data()
@@ -11,23 +13,28 @@ regression = train_model(X, Y)
 print("Training Completed")
 
 def validate_data(all_locations, data):
-    location = data["location"]
-    size = data["size"]
-    area = data["area"]
-    bath = data["bath"]
-    error = False
-    if all_locations.count(location) == 0 or \
-            not (0 < size <= 50) or \
-            not ((size * 300) < area < (size * 600)) or \
-            not (0 < bath <= size):
-        error = True
-    return error, location, size, area, bath
+    try:
+        location = data["location"]
+        size = int(data["size"])
+        area = int(data["area"])
+        bath = int(data["bath"])
+        error = False
+        if all_locations.count(location) == 0 or \
+                not (0 < size <= 50) or \
+                not ((size * 300) <= area <= (size * 600)) or \
+                not (0 <= bath <= size):
+            error = True
+        return error, location, size, area, bath
+    except ValueError:
+        return True, "", 0, 0, 0
 
 @app.route("/locations", methods=["GET"])
+@cross_origin(supports_credentials=True)
 def get_locations():
     return jsonify({"locations": locations})
 
 @app.route('/predict-price', methods=['POST'])
+@cross_origin(supports_credentials=True)
 def predict_house_price():
     error, location, size, area, bath = validate_data(locations, request.get_json())
     if error:
